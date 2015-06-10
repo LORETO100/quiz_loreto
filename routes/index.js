e9742c9d918a37ca7dd9fce6cdbc1e34640b56cd
loreto <loreto.gomez@coitim.es>
2015-06-10 08:15:18 +0200

    Despliegue DB en Heroku (finalizado)

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,17 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller')
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index.ejs', { title: 'Quiz' });
+// Pagina de entrada (home page)
+router.get('/',function(req,res){
+		res.render('index',{title:'Quiz'});
 });
 
-router.get('/author',quizController.author);
-router.get('/quizes/question',quizController.question);
-router.get('/quizes/answer',quizController.answer);
+//Autoload de comandos con:quizId
+router.param('quizId',quizController.load);//autoload:quizId
+
+// Definicion de rutas de /quizes
+router.get('/quizes',quizController.index);
+router.get('/quizes/:quizId(\\d+)',quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
 
 module.exports = router;
